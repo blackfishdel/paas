94dafa01117e977393cafdd360bbd7f084009d4a
xiepeng <xiepeng@5173.com>
2018-02-05 12:56:13 +0800

    工作日志

--- 工作脚本/maven-build.sh
+++ 工作脚本/maven-build.sh
@@ -3,20 +3,30 @@
 #
 #
 #
+# snapshot
 cd /Users/xiepeng/Documents/GitHub/super-bom && \
 mvn clean install deploy -U -Dmaven.test.skip=true \
 -DaltSnapshotDeploymentRepository="m5173::default::http://61.130.29.7/nexus/content/repositories/snapshots/"
 
+
+# release
 cd /Users/xiepeng/Documents/GitHub/super-bom && \
 mvn deploy -U -Dmaven.test.skip=true \
--DaltReleaseDeploymentRepository="m5173::default::http://61.130.29.7/nexus/content/repositories/snapshots/"
+-DaltReleaseDeploymentRepository="m5173::default::http://61.130.29.7/nexus/content/repositories/release/"
 
-cd /Users/xiepeng/Documents/GitHub/base-project
+# 显示引用jar包为什么被使用
+mvn dependency:tree -Dverbose -Dincludes=aspectjweaver
+
+mvn clean install  && \
+cd ./sso-support-starter && \
+mvn deploy -U -Dmaven.test.skip=true \
+-DaltSnapshotDeploymentRepository="m5173::default::http://61.130.29.7/nexus/content/repositories/snapshots/"
+
 cd ./sso-service && \
 mvn clean install -U -Dmaven.test.skip=true && \
 cd ./sso-support-service && \
 mvn docker:build -DpushImage
-
+192.168.140.90:5000/sso-support-service:0.0.1-SNAPSHOT
 
 docker tag davidcaste/alpine-tomcat:jre8tomcat8 192.168.140.90:5000/alpine-tomcat:jre8tomcat8
 docker push 192.168.140.90:5000/alpine-tomcat:jre8tomcat8
---
+++ 知识积累/atom bug解决.md
@@ -0,0 +1,8 @@
+
+## atom project不显示目录
+1. Save any unsaved work first
+1. Close all open Atom projects
+1. Run `atom --clear-window-state` saw on this reference
+1. Run `atom --safe` on the repo that I had issues with (file tree now shows up)
+1. Close that repo project in Atom
+1. Run `atom .` on the same repo (file tree still shows up)
